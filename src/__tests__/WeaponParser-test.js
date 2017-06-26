0f2b5dca2e60a9134b2b573425f3c542094b675e
Pontus Persson <pontus.alexander@gmail.com>
2017-06-26 14:35:18 +0200

    Update WeaponTest.

---
+++ src/__tests__/WeaponParser-test.js
@@ -0,0 +1,42 @@
+const expect = require('expect.js');
+const sinon = require('sinon');
+const {createNode} = require('./helpers/xmlreader');
+
+const {Entity, Loader, Objects} = require('@snakesilk/engine');
+const WeaponParser = require('../WeaponParser');
+
+describe('WeaponParser', function() {
+    let parser, Weapon;
+
+    beforeEach(() => {
+        parser = new WeaponParser(new Loader());
+    });
+
+    [
+        'AirShooter',
+        'CrashBomber',
+        'EnemyPlasma',
+        'MetalBlade',
+        'Plasma',
+        'TimeStopper',
+    ].forEach(name => {
+        const id = name + '-id';
+        const code = name[0];
+        const projectileId = name + '-projectile';
+        const xmlString = `<weapon source="${name}" id="${id}" name="${name}-name" code="${code}" ammo="28" cost="1.75">
+            <projectile id="${projectileId}" amount="3"/>
+        </weapon>`;
+
+        describe(`when parsing ${xmlString}`, () => {
+            beforeEach(() => {
+                parser.loader.resourceManager.addObject(projectileId, Entity);
+                const node = createNode(xmlString);
+                Weapon = parser.parseWeapon(node);
+            });
+
+            it(`produces a ${name} weapon`, () => {
+                expect(Weapon).to.be.a(Function);
+            });
+        });
+    });
+});

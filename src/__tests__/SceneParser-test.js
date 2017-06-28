753b03824c8766d27ac6b780e65d893f000ea5f5
Pontus Persson <pontus.alexander@gmail.com>
2017-06-28 18:57:48 +0200

    Refactor SceneParser test.

--- src/__tests__/SceneParser-test.js
+++ src/__tests__/SceneParser-test.js
@@ -3,97 +3,204 @@ const sinon = require('sinon');
 const fs = require('fs');
 
 const mocks = require('@snakesilk/testing/mocks');
-const {readXMLFile} = require('@snakesilk/testing/xml');
+const {createNode, readXMLFile} = require('@snakesilk/testing/xml');
 
 const THREE = require('three');
 const {Game, Loader, World} = require('@snakesilk/engine');
 const SceneParser = require('../SceneParser');
 
-describe('SceneParser', function() {
-  let parser, node;
+describe('SceneParser', () => {
+  let scene;
 
-  before(() => {
-    node = readXMLFile(__dirname + '/fixtures/scene.xml').childNodes[0];
-  });
+  class AudioMock{};
 
-  beforeEach(function() {
-    mocks.AudioContext.mock();
-    mocks.Image.mock();
+  function parseScene(parser) {
+    return parser.getScene().then(_s => {scene = _s});
+  }
 
-    const loader = new Loader();
-    sinon.stub(loader.resourceLoader, 'loadImage', () => {
-      return Promise.resolve(new mocks.Canvas())
+  describe(`when <audio> present`, () => {
+    const mockAudio = new AudioMock();
+    let loader;
+
+    beforeEach(() => {
+      const node = createNode(`<scene>
+        <audio>
+          <music src="./intro/intro.ogg" id="intro"/>
+        </audio>
+      </scene>`);
+      loader = new Loader();
+      sinon.stub(loader.resourceLoader, 'loadAudio', () => {
+        return Promise.resolve(mockAudio);
+      });
+
+      const parser = new SceneParser(loader, node);
+      return parseScene(parser);
     });
 
-    sinon.stub(World.prototype, 'simulateTime');
+    it('fetches audio from "src"', () => {
+      expect(loader.resourceLoader.loadAudio.callCount).to.be(1);
+      expect(loader.resourceLoader.loadAudio.lastCall.args)
+        .to.eql(['./intro/intro.ogg']);
+    });
 
-    parser = new SceneParser(loader, node);
+    it('adds audio to scene', (done) => {
+      scene.audio.events.bind(scene.audio.EVENT_PLAY, (audio) => {
+        expect(audio).to.be(mockAudio);
+        done();
+      });
+      scene.audio.play('intro');
+    });
   });
 
-  afterEach(function() {
-    mocks.AudioContext.restore();
-    mocks.Image.restore();
-
-    World.prototype.simulateTime.restore();
-  });
-
-  describe('when instantiated', () => {
-    describe('#getScene', () => {
-      let scene;
-
+  [
+    ['solids', 'solid'],
+    ['deathzones', 'deathZone'],
+    ['climbables', 'climbable'],
+  ].forEach(([tag, trait]) => {
+    describe(`when <behaviors><${tag}> present`, () => {
       beforeEach(() => {
-        return parser.getScene().then(_s => {scene = _s});
+        const node = createNode(`<scene>
+          <layout>
+            <behaviors>
+              <${tag}>
+                <rect x="136" y="-184" w="240" h="16"/>
+                <rect x="1976.23" y="-408.2112" w="16.5" h="32.23"/>
+              </${tag}>
+            </behaviors>
+          </layout>
+        </scene>`);
+        const parser = new SceneParser(new Loader(), node);
+        return parseScene(parser);
       });
 
-      it('creates objects with valid positions', function() {
-        scene.world.objects.forEach(function(object) {
-          expect(object.position).to.be.a(THREE.Vector3);
-          expect(object.position.x).to.be.a('number');
-          expect(object.position.y).to.be.a('number');
-          expect(object.position.z).to.be.a('number');
+      it(`has only ${tag} in world`, () => {
+        const objects = scene.world.objects.filter(o => o[trait]);
+        expect(objects).to.have.length(2);
+      });
+
+      it('honors positions and sizes', () => {
+        expect(scene.world.objects[0].position).to.eql({ x: 136, y: -184, z: 0 });
+        expect(scene.world.objects[0].collision[0].w).to.eql(240);
+        expect(scene.world.objects[0].collision[0].h).to.eql(16);
+
+        expect(scene.world.objects[1].position).to.eql({ x: 1976.23, y: -408.2112, z: 0 });
+        expect(scene.world.objects[1].collision[0].w).to.eql(16.5);
+        expect(scene.world.objects[1].collision[0].h).to.eql(32.23);
+      });
+    });
+  });
+
+  describe('when parsing full scene', () => {
+    let node;
+
+    const mockAudio = new AudioMock();
+
+    before(() => {
+      node = readXMLFile(__dirname + '/fixtures/scene.xml').childNodes[0];
+    });
+
+    beforeEach(() => {
+      mocks.AudioContext.mock();
+      mocks.Image.mock();
+      mocks.requestAnimationFrame.mock();
+      mocks.THREE.WebGLRenderer.mock();
+
+      const loader = new Loader(new Game());
+      sinon.stub(loader.resourceLoader, 'loadImage', () => {
+        return Promise.resolve(new mocks.Canvas())
+      });
+      sinon.stub(loader.resourceLoader, 'loadAudio', () => {
+        return Promise.resolve(mockAudio);
+      });
+
+      sinon.stub(World.prototype, 'simulateTime');
+
+      const parser = new SceneParser(loader, node);
+      return parseScene(parser);
+    });
+
+    afterEach(() => {
+      mocks.AudioContext.restore();
+      mocks.Image.restore();
+      mocks.requestAnimationFrame.restore();
+      mocks.THREE.WebGLRenderer.restore();
+
+      World.prototype.simulateTime.restore();
+    });
+
+    describe('when instantiated', () => {
+      describe('#getScene', () => {
+        it('creates objects with valid positions', () => {
+          scene.world.objects.forEach(object => {
+            expect(object.position).to.be.a(THREE.Vector3);
+            expect(object.position.x).to.be.a('number');
+            expect(object.position.y).to.be.a('number');
+            expect(object.position.z).to.be.a('number');
+          });
         });
-      });
 
-      it.skip('creates sane textures', () => {
-        expect(object.model.material.map).to.be.a(THREE.Texture);
-      });
-
-      it('should not put any objects in scene without texture ', function() {
-        scene.world.scene.children.forEach(function(mesh) {
-          if (mesh.material && !mesh.material.map) {
-            console.error('Mesh missing texture', mesh);
-          }
-        });
-      });
-
-      describe('parsed Camera', function() {
-        it('have smoothing', function() {
-          expect(scene.camera.smoothing).to.equal(13.5);
+        it('creates DeathZones', () => {
+          const deathZones = scene.world.objects.filter(object => object.deathZone);
+          expect(deathZones).to.have.length(3);
         });
 
-        it('have paths', function() {
-          const paths = scene.camera.paths;
-          expect(paths).to.have.length(3);
-          expect(paths[0].window[0]).to.eql({x: 0, y: -208, z: 0});
-          expect(paths[0].window[1]).to.eql({x: 2048, y: 0, z: 0});
-          expect(paths[0].constraint[0]).to.eql({x: 180, y: -120, z: 150});
-          expect(paths[0].constraint[1]).to.eql({x: 1920, y: -120, z: 150});
+        it('creates Ladders', () => {
+          const ladders = scene.world.objects.filter(object => object.climbable);
+          expect(ladders).to.have.length(2);
         });
-      });
 
-      describe('Object Parsing', function() {
-        it('should name object', function() {
-          const object = scene.world.getObject('my-test-object');
-          expect(object.id).to.equal('my-test-object');
-          expect(object.name).to.equal('test');
+        it('creates Solids', () => {
+          const solids = scene.world.objects.filter(object => object.solid);
+          expect(solids).to.have.length(5);
         });
-      });
 
-      it.skip('should parse checkpoints', function() {
-        expect(scene.checkPoints).to.have.length(3);
-        expect(scene.checkPoints[0]).to.eql({pos: {x: 136, y: -165}, radius: 100});
-        expect(scene.checkPoints[1]).to.eql({pos: {x: 1920, y: -661}, radius: 100});
-        expect(scene.checkPoints[2]).to.eql({pos: { x: 4736, y: -1109}, radius: 13});
+        it('should not put any objects in scene without texture ', () => {
+          scene.world.scene.children.forEach(function(mesh) {
+            if (mesh.material && !mesh.material.map) {
+              console.error('Mesh missing texture', mesh);
+            }
+          });
+        });
+
+        describe('Audio', () => {
+          it('was parsed', (done) => {
+            scene.audio.events.bind(scene.audio.EVENT_PLAY, (audio) => {
+              expect(audio).to.be(mockAudio);
+              done();
+            });
+            scene.audio.play('intro');
+          });
+        });
+
+        describe('Camera', () => {
+          it('have smoothing', () => {
+            expect(scene.camera.smoothing).to.equal(13.5);
+          });
+
+          it('have paths', () => {
+            const paths = scene.camera.paths;
+            expect(paths).to.have.length(3);
+            expect(paths[0].window[0]).to.eql({x: 0, y: -208, z: 0});
+            expect(paths[0].window[1]).to.eql({x: 2048, y: 0, z: 0});
+            expect(paths[0].constraint[0]).to.eql({x: 180, y: -120, z: 150});
+            expect(paths[0].constraint[1]).to.eql({x: 1920, y: -120, z: 150});
+          });
+        });
+
+        describe('Object Parsing', () => {
+          it('should name object', () => {
+            const object = scene.world.getObject('my-test-object');
+            expect(object.id).to.equal('my-test-object');
+            expect(object.name).to.equal('test');
+          });
+        });
+
+        it.skip('should parse checkpoints', () => {
+          expect(scene.checkPoints).to.have.length(3);
+          expect(scene.checkPoints[0]).to.eql({pos: {x: 136, y: -165}, radius: 100});
+          expect(scene.checkPoints[1]).to.eql({pos: {x: 1920, y: -661}, radius: 100});
+          expect(scene.checkPoints[2]).to.eql({pos: { x: 4736, y: -1109}, radius: 13});
+        });
       });
     });
   });

523299e49a826d5a8d01366fb014afe463ca4d1d
Pontus Persson <pontus.alexander@gmail.com>
2017-06-30 07:12:21 +0200

    Export Parser, ObjectParser, and SceneParser.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const XMLLoader = require('./XMLLoader');
+const Parser = require('./parsers/Parser');
+
+Parser.ObjectParser = require('./parsers/ObjectParser');
+Parser.SceneParser = require('./parsers/SceneParser');
 
 module.exports = {
     XMLLoader,
+    Parser,
 };

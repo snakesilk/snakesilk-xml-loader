b45c2dee954bcffed60ece0d7cee29844679fa8a
Pontus Persson <pontus.alexander@gmail.com>
2017-07-14 17:23:08 +0200

    Add XMLLoader test placeholder.

---
+++ src/__tests__/XMLLoader-test.js
@@ -0,0 +1,8 @@
+const expect = require('expect.js');
+const sinon = require('sinon');
+const XMLLoader = require('../XMLLoader');
+
+describe('XMLLoader', function() {
+  describe.skip('#loadScene', () => {
+  });
+});

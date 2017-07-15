24b4f0ddcda772914982537f0bfd124631d0f99c
Pontus Persson <pontus.alexander@gmail.com>
2017-07-15 16:14:15 +0200

    Add regression test for XML loader.

--- src/__tests__/XMLLoader-test.js
+++ src/__tests__/XMLLoader-test.js
@@ -2,7 +2,20 @@ const expect = require('expect.js');
 const sinon = require('sinon');
 const XMLLoader = require('../XMLLoader');
 
-describe('XMLLoader', function() {
-  describe.skip('#loadScene', () => {
-  });
+describe('XMLLoader', () => {
+    describe('on instantiation', () => {
+        let xmlLoader;
+
+        beforeEach(() => {
+            xmlLoader = new XMLLoader();
+        });
+
+        it('has no method loadScene()', () => {
+            expect(xmlLoader.loadScene).to.be(undefined);
+        });
+
+        it('has no method parseScene()', () => {
+            expect(xmlLoader.loadScene).to.be(undefined);
+        });
+    });
 });
